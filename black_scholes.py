a6190440f714ce356b0f6744dda2a872dbdaa779
dray89 <46060603+dray89@users.noreply.github.com>
2019-07-06 19:10:18 -0400

    Update black_scholes.py

--- black_scholes.py
+++ black_scholes.py
@@ -8,40 +8,54 @@ Valuation of European call options in Black-Sholes Merton model.
 incl. Vega function and implied volatility estimation
 bsm_functions.py
 
-***from pythonforfinanceebook
+Black Scholes is a pricing model 
+***from pythonforfinance book
 """
+from math import log, sqrt, exp
+from scipy import stats
 
-def bsm_call_values():
+def bsm_call_values(initial_p,strike,maturity,vol_diff,sigma):
     ''' Valuation of European call option in BSM model.
     Analytical Formula.
     
     Parameters
     ===========
-    S0 : float
-        initial stock/index level
-    K : float
-        strike price
-    T : float
-        strike price
-    r : float
-        volatility factor in diffusion term
+    initial_p : float
+                initial stock/index level
+    strike    : float
+                strike price
+    maturity  : float
+                maturity date (in year fractions)
+    risk_free : float
+                constant risk-free short rate
+    vol_diff  : float
+                volatility factor in diffusion term
     
     Returns
     =======
     value : float
         present value of the European call option
     '''
-
-    from math import log sqrt, exp
-    from scipy import stats
-
-    S0 = float(S0)
-    d1 = (log(S0 / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-    d2 = (log(S0/K) + (r - 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
-
-    value = (S0 * stats.norm.cdf(d1, 0.0, 1.0)
-        - K*exp(-r * T) * stats.norm.cdf(d2, 0.0, 1.0))
+    initial_p = float(initial_p)
+    d1 = (log(initial_p/strike) + (vol_diff + 0.5 * sigma ** 2) * maturity) / (sigma * sqrt(maturity))
+    d2 = (log(initial_p/strike) + (risk_free - 0.5 * sigma ** 2) * maturity) / (sigma * sqrt(maturity))
+    value = (initial_p * stats.norm.cdf(d1, 0.0, 1.0)
+        - strike*exp(-vol_diff * maturity) * stats.norm.cdf(d2, 0.0, 1.0))
 
 # stats.norm.cdf -> cumulative distribution function for normal distribution
 
     return value
+
+'''
+
+Greeks
+=======
+Delta: Probability an option will expire in the money
+        Elasticity of option price to the price of the underlying
+        Call options = positive delta between 0 and 1, Delta approaches 1 as time increases
+        Put options = negative delta between 0 and -1, Delta approaches -1 as time increases
+Gamma: How much delta will change when the stock price changes
+        The rate of change of delta, Gamma decreases as Delta approaches 1 because Delta's rate of change slows approaching 1
+Theta: Time loss
+Vega: Option price sensitivity to underlying stock
+Rho: Sensitivity to changes in bond prices 
