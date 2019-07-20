a63f642d468ebe99acdd7d201499f7dd130c33c7
Debra Ray <raydebra89@gmail.com>
2019-07-20 10:13:27 -0400

    update weed.py

--- weed.py
+++ weed.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from pandas import DataFrame
 from addtl_fun import addtl, balance_sheet
 
-symbol = "weed.to"
+symbol = "WEED.TO"
 source = "yahoo"
 start = datetime(2018, 7, 19)
 end = datetime(2019, 7, 19)
@@ -18,9 +18,6 @@ weed = calculations(symbol, source, start, end)
 ''' clean data '''
 '''should be nan'''
 weed.dividend 
-df = weed.df()
-
-df.to_csv('weed.csv')
 
 price = weed.stock
 div = weed.dividend
@@ -28,17 +25,17 @@ chg_pct = ((price['Close']['2019-03-29'] - price['Close']['2019-07-18'])/price['
 
 bsd_weed = balance_sheet(symbol, source, start, end)
 
-bsd_weed.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][0]['2019-03-31']['retainedEarnings']
-n = bsd_weed.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][0]['2019-03-31']['longTermDebt'] 
+bsd_weed.retained_earnings
+bsd_weed.debt
 b = bsd_weed.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][3]['2018-06-30']['longTermDebt']
 chg_debt_weed = n-b
 
-n = bsd.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][0]['2019-03-31']['netReceivables'] 
-b = bsd.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][3]['2018-06-30']['netReceivables']
+bsd_weed.netrec
+b = bsd_weed.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][3]['2018-06-30']['netReceivables']
 rec_weed = n - b
 
-n = bsd.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][0]['2019-03-31']['totalStockholderEquity'] 
-b = bsd.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][3]['2018-06-30']['totalStockholderEquity']
+bsd_weed.equity
+b = bsd_weed.bsd['balanceSheetHistoryQuarterly'][symbol.upper()][3]['2018-06-30']['totalStockholderEquity']
 eq_weed = n-b
 
 d_r_weed = chg_debt_weed/rec_weed
