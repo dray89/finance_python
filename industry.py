196984177b98ed1e1b8a307e85e68efc73c95403
Debra Ray <raydebra89@gmail.com>
2019-08-22 17:13:17 -0400

    publish

--- industry.py
+++ industry.py
@@ -14,53 +14,33 @@ except:
 balance_sheets = lambda x, y: {y: x[range(x)][y].bsd}
 
 class industry:
-        def __init__(self, df_list):
-            self.df_list = df_list
-            self.__concat_df__()
-            self.attributes = ['ind', 'maximum', 'minimum', 'max_list', 
-                               'min_list', 'max_sentence', 'min_sentence']
-        
-        def __concat_df__(self):
-            ind = pandas.concat(self.df_list, axis=1).dropna(how='all')
-            ind = ind.astype(float, errors='ignore')
-            try:
-                averages = ind.mean(axis=1).dropna(how='all')
-                ind['Averages'] = averages
-            except:
-                print('Error in Industry - Concat_df, Try Reformatting')
-                self.df_list = list(map(lambda each: clean_dfs.clean_stats(each), self.df_list))
-                ind = pandas.concat(self.df_list, axis=1).dropna(how='all')
-                ind = ind.astype(float, errors='ignore')
-                averages = ind.mean(axis=1).dropna(how='all')
-                ind['Averages'] = averages
-            finally:
-                self.ind = ind
+    min_list = []
+    max_list = []
             
-        def analyze_chgs(self):
-            for a, b in enumerate(self.ind):
-                if hasattr(b, 'changes'):
-                    pass
-                else:
-                    self.ind.pop(a)
-                    continue
-            frame = list(map(lambda x: pandas.DataFrame(self.ind[x].changes), range(0, len(self.ind))))
-            self.maximum = frame.idxmax(axis = 1)
-            self.minimum = frame.idxmin(axis=1)
-            self.max_list = []
+    def industry_changes(self):
+        for a, b in enumerate(self.ind):
+            if hasattr(b, 'changes'):
+                pass
+            else:
+                self.ind.pop(a)
+                continue
+        frame = list(map(lambda x: pandas.DataFrame(self.ind[x].changes), range(0, len(self.ind))))
+        self.maximum = frame.idxmax(axis = 1)
+        self.minimum = frame.idxmin(axis=1)
 
-            for c, d in enumerate(frame.idxmax(axis=1)):
-                i = frame.idxmax(axis=1).index[c]
-                symbol = frame.idxmax(axis=1)[i]
-                self.max_list.append({symbol: [i, frame[d].T[i]]})
-            self.min_list = []
 
-            for e, f in enumerate(frame.idxmin(axis=1)):
-                i = frame.idxmin(axis=1).index[e]
-                symbol = frame.idxmin(axis=1)[i]
-                self.min_list.append({symbol: [i, frame[f].T[i]]})
+        for c, d in enumerate(frame.idxmax(axis=1)):
+            i = frame.idxmax(axis=1).index[c]
+            symbol = frame.idxmax(axis=1)[i]
+            self.max_list.append({symbol: [i, frame[d].T[i]]})
 
-            self.max_sentence = "{0} increased its {1} by {2} in the past year."
-            self.min_sentence =  "{0} decreased its {1} by {2}."
+        for e, f in enumerate(frame.idxmin(axis=1)):
+            i = frame.idxmin(axis=1).index[e]
+            symbol = frame.idxmin(axis=1)[i]
+            self.min_list.append({symbol: [i, frame[f].T[i]]})
+
+        self.max_sentence = "{0} increased its {1} by {2} in the past year."
+        self.min_sentence =  "{0} decreased its {1} by {2}."
         
         def print_sentences(self):
             for each in enumerate(self.max_list):
