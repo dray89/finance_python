b199a6c447054bf6312c0b592fe2898d72979b21
Debra Ray <raydebra89@gmail.com>
2019-09-15 16:00:35 -0400

    updating screener

--- sectors.py
+++ sectors.py
@@ -14,12 +14,12 @@ class sector:
         "Consumer_Cyclical", "Consumer_Defensive", "Energy", 
         "Financial_Services", "Healthcare", "Industrials", "Real_Estate", 
         "Technology", "Utilities"]'''
-        self.sector = sector
-        file = "{0}.txt".format(sector.lower())
+        self.sector = sector.lower()
+        file = "{0}.txt".format(self.sector)
         self.file = open(file, 'r')
-        self.contents = self.clean_contents()
+        self.contents = self.__cleancontents__()
 
-    def clean_contents(self):
+    def __cleancontents__(self):
         contents = self.file.readlines()
         new_list = []
         for each in contents:
@@ -33,5 +33,51 @@ class sector:
         for index, item in enumerate(self.contents):
             if symbol in item:
                 print(self.contents[index+1])
+
+    def symbol_lookup(self, industry):
+        '''Basic_Materials = Agriculture, Building Materials, Chemicals, Coal, Forest Products, Metals & Mining, Steel     
+            
+            Communication_Services = Communication_Services
+            
+            Consumer_Cyclical = Advertising & Marketing Services, Autos, Entertainment, Homebuilding & Construction, Manufacturing - Apparel/Furniture, Packaging & Containers, Personal Services, Publishing, Restaurants, Retail - Apparel & Specialty, Travel & Leisure	 
+            
+            Consumer_Defensive = Beverages - Alcoholic, Beverages - Non-Alcoholic, Consumer Packaged Goods, Education, Retail - Defensive, Tobacco Product
+            
+            Energy = Oil & Gas - Drilling, Oil & Gas - E&P, Oil & Gas - Integrated, Oil & Gas - Midstream, Oil & Gas - Refining & Marketing, Oil & Gas - Services
+            
+            Financial_Services = Asset Management, Banks, Brokers & Exchanges, Credit Services, Insurance, Insurance - Life, Insurance - Property & Casualty, Insurance - Specialty	 
+
+            Healthcare = Biotechnology, Drug Manufacturers, Health Care Plans, Health Care Providers, Medical Devices, Medical Diagnostics & Research, Medical Distribution, Medical Instruments & Equipment	
+
+            Industrials = Aerospace & Defense, Airlines, Business Services, Conglomerates, Consulting & Outsourcing, Employment Services, Engineering & Construction, Farm & Construction Machinery	
+            
+            Real_Estate = REITs, Real Estate Services
+
+            Technology =  Application Software, Communication Equipment, Computer Hardware, Online Media, Semiconductors
+
+            Utilities = Independent Power Producers, Utilities - Regulated
+
+            '''
+        symbol_list = []
+        for index, item in enumerate(self.contents):
+            if industry in item:
+                symbol_list.append(self.contents[index-1])
+        return symbol_list
+
+    def get_industries(self):
+        dictionary = {'basic_materials':"Agriculture, Building Materials, Chemicals, Coal, Forest Products, Metals & Mining, Steel",
+                      "communication_services":"Communication_Services",
+                      "consumer_cyclical":"Advertising & Marketing Services, Autos, Entertainment, Homebuilding & Construction, Manufacturing - Apparel/Furniture, Packaging & Containers, Personal Services, Publishing, Restaurants, Retail - Apparel & Specialty, Travel & Leisure",
+                      "consumer_defensive":"Beverages - Alcoholic, Beverages - Non-Alcoholic, Consumer Packaged Goods, Education, Retail - Defensive, Tobacco Product",
+                      "energy":"Oil & Gas - Drilling, Oil & Gas - E&P, Oil & Gas - Integrated, Oil & Gas - Midstream, Oil & Gas - Refining & Marketing, Oil & Gas - Services",
+                      "financial_Services":"Asset Management, Banks, Brokers & Exchanges, Credit Services, Insurance, Insurance - Life, Insurance - Property & Casualty, Insurance - Specialty",
+                      "healthcare":"Biotechnology, Drug Manufacturers, Health Care Plans, Health Care Providers, Medical Devices, Medical Diagnostics & Research, Medical Distribution, Medical Instruments & Equipment",
+                      "industrials":"Aerospace & Defense, Airlines, Business Services, Conglomerates, Consulting & Outsourcing, Employment Services, Engineering & Construction, Farm & Construction Machinery",
+                      "real_Estate":"REITs, Real Estate Services",
+                      "technology":"Application Software, Communication Equipment, Computer Hardware, Online Media, Semiconductors",
+                      "utilities":"Independent Power Producers, Utilities - Regulated" }
+
+        return dictionary[self.sector]
+
 
 
