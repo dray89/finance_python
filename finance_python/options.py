12aa0bec078193ef61bdeca20dc75c460b2f5ba2
Debra Ray <raydebra89@gmail.com>
2019-11-25 17:00:25 -0500

    created two new classes
    created sell calls and sell puts classes

--- finance_python/options.py
+++ finance_python/options.py
@@ -13,7 +13,6 @@ except:
     from finance_python.scrapers import scraper
     from finance_python.headers import headers
     
-from datetime import datetime
 import calendar
 from datetime import date, timedelta
 import pandas as pd 
@@ -21,7 +20,7 @@ from multiprocessing import Pool
 
 class options(stock):
     def utc_dates(self, year_series):
-        dt = year_series.apply(lambda x: datetime.combine(x, datetime.min.time()))
+        dt = year_series.apply(lambda x: datetime.datetime.combine(x, datetime.datetime.min.time()))
         utc_dates = dt.apply(lambda x: x.tz_localize('utc').timetuple())
         strptime_date = utc_dates.apply(lambda x: calendar.timegm(x))
         to_string = strptime_date.apply(lambda x: str(x))
@@ -52,24 +51,68 @@ class options(stock):
         y = len(utc_dates)
         while y > 0:
             for x in utc_dates:
-                if len(acb.options(x))>0:
+                if len(T.options(x))>0:
                     yield x
                 y-=1
-                
+  
+class sell_calls(options):
+    def break_even(self, table):
+        break_even = table['Strike'] - table['Last Price']
+        return break_even
+    
+    def discount(self, table):
+        disc_prem = table['Break Even'].apply(lambda x: x - float(self.price))
+        return disc_prem
+    
+    def create_df(self, table):
+        table['Break Even'] = self.break_even(table)
+        table['Discount/Premium'] = self.discount(table)
+        r = table[['Discount/Premium' , 'Break Even', 'Strike', 'Last Price']]
+        r = r.sort_values('Discount/Premium', ascending=True)
+        r = r.set_index('Strike')
+        return r 
+    
+class sell_puts(options):
+    def break_even(self, table):
+        break_even = table['Strike'] - table['Last Price.1']
+        return break_even
+    
+    def discount(self, table):
+        disc_prem = table['Break Even'].apply(lambda x: x - float(self.price))
+        return disc_prem
+    
+    def create_df(self, table):
+        table['Break Even'] = self.break_even(table)
+        table['Discount/Premium'] = self.discount(table)
+        r = table[['Break Even', 'Discount/Premium', 'Strike', 'Last Price.1']]
+        r = r.sort_values('Discount/Premium', ascending=True)
+        r = r.set_index('Strike')
+        return r
+    
 if __name__ == '__main__':
     start = date.today() - timedelta(days=365*15)
     end = date.today()
 
-    acb = options('acb', start, end)
+    T = options('T', start, end)
 
-    month = acb.option_dates(2019)
-    year_2019 = acb.third_fridays(month)
-    year_2020 = acb.option_dates(2020)
-    yr2020 = acb.third_fridays(year_2020)
+    month = T.option_dates(2019)
+    year_2019 = T.third_fridays(month)
+    year_2020 = T.option_dates(2020)
+    yr2020 = T.third_fridays(year_2020)
     yr2020.append(year_2019[-1])
     yr2020 = pd.Series(yr2020)
-    utc_dates = list(acb.utc_dates(yr2020))
-    a  = list(acb.bad_dates(utc_dates))
+    utc_dates = list(T.utc_dates(yr2020))
+    
+    a  = list(T.bad_dates(utc_dates))
+    dictionary = dict(zip(yr2020,utc_dates))
+    december_option = dictionary.get(datetime.date(2019, 12, 20))
+    
+    table_T = T.options(str(december_option))
+    
+    table_T = T.break_even(table_T)
+    
+    table_T = T.discount(table_T)
+
     
     #p = Pool()
-    #price = list(p.map(acb.options, a))
+    #price = list(p.map(T.options, a))
