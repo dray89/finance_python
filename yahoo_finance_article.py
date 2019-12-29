3c61388812ad031a9c74e759fa0b17698891d0f8
Debra Ray <raydebra89@gmail.com>
2019-12-29 13:26:39 -0500

    Update yahoo_finance_article.py

--- yahoo_finance_article.py
+++ yahoo_finance_article.py
@@ -11,20 +11,21 @@ import requests, pandas, lxml
 from lxml import html
 
 def scrape_page(url, hdrs):
-    page = requests.get(url, params=hdrs)
-    tree = html.fromstring(page.content)
-    table = tree.xpath('//table')
-    table = list(map(lambda x: pandas.read_html(lxml.etree.tostring(table[x], method='xml'))[0], range(0,len(table))))
-    return table
+     page = requests.get(url, params=hdrs)
+     element_html = html.fromstring(page.content)
+     table = element_html.xpath('//table')
+     table_tree = lxml.etree.tostring(table[0], method='xml')
+     panda = pandas.read_html(table_tree)
+     return panda
 
 def format_date(date_datetime):
-     date_timetuple = date_datetime.timetuple()
-     date_mktime = time.mktime(date_timetuple)
-     date_int = int(date_mktime)
-     date_str = str(date_int)
-     return date_str
+    date_timetuple = date_datetime.timetuple()
+    date_mktime = time.mktime(date_timetuple)
+    date_int = int(date_mktime)
+    date_str = str(date_int)
+    return date_str
  
-def subdomain(symbol, start, end, filter='History'):
+def subdomain(symbol, start, end, filter='history'):
     '''
 
     Parameters
@@ -44,8 +45,8 @@ def subdomain(symbol, start, end, filter='History'):
     subdomain = subdoma.format(symbol, start, end, filter)
     return subdomain
  
-def history_hdr(subdomain):
-     hdrs =  {"authority": "finance.yahoo.com",
+def header_function(subdomain):
+    hdrs =  {"authority": "finance.yahoo.com",
                  "method": "GET",
                  "path": subdomain,
                  "scheme": "https",
@@ -54,26 +55,33 @@ def history_hdr(subdomain):
                  "accept-language": "en-US,en;q=0.9",
                  "cache-control": "no-cache",
                  "cookie": "Cookie:identifier",
-                 "dnt": 1,
+                 "dnt": "1",
                  "pragma": "no-cache",
                  "sec-fetch-mode": "navigate",
                  "sec-fetch-site": "same-origin",
                  "sec-fetch-user": "?1",
-                 "upgrade-insecure-requests": 1,
-                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64)
-     return hdrs
+                 "upgrade-insecure-requests": "1",
+                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64)"}
+    return hdrs
  
 if __name__ == '__main__':
-     symbol = 'BB'
+    base_url = 'https://finance.yahoo.com'
+    symbol = 'BB'
      
-     dt_start = datetime.today() - timedelta(days=365)
-     dt_end = datetime.today()
-    
-     start = format_date(dt_start)
-     end = format_date(dt_end)
+    dt_start = datetime.today() - timedelta(days=1000)
+    dt_end = datetime.today()
        
-     subdoma = "/quote/{0}/history?period1={1}&period2= 
-               {2}&interval=1d&filter=history&frequency=1d"
-     
-     subdomain = subdoma.format(symbol, start, end)
-     header = history_hdr(subdomain)
+    start = format_date(dt_start)
+    end = format_date(dt_end)
+      
+    sub = subdomain(symbol, start, end)
+    hdrs = header_function(sub)
+    
+    url = base_url + sub
+    
+    page = requests.get(url, headers=hdrs)
+    element_html = html.fromstring(page.content)
+    table = element_html.xpath('//*[@id="Col1-1-HistoricalDataTable-Proxy"]/section/div[2]/table')
+    table_tree = lxml.etree.tostring(table[0], method='HTML', pretty_print=True)
+    panda = pandas.read_html(table_tree)
+    
