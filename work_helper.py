da1ba54b4238911bbe135129c1e80796574fce98
Debra Ray <raydebra89@gmail.com>
2020-10-17 10:43:32 -0500

    Changed the name of some files

--- work_helper.py
+++ work_helper.py
@@ -8,11 +8,11 @@ Created on Wed Oct 14 13:55:10 2020
 from finance_python.statistics import statistics
 from finance_python.stock import stock
 
-template_down = "{0} (TSX:{1}) fell to {2} during the March market sell-off from a 52-week high of {3}. At the time of writing, investors are trading the stock for {4} per share. The annual dividend yield would be a great addition to your retirement portfolio at {5}."
-template_up = "{0} (TSX:{1}) rose from a 52-week low of {2} to a 52-week high of {3} after the March 2020 market sell-off. At the time of writing, the stock is trading for {4} per share. The dividend yield is {5} annually."
+template_down = "{0} (TSX:{1}) fell to ${2} during the March market sell-off from a 52-week high of ${3}. At the time of writing, investors are trading the stock for ${4} per share. The annual dividend yield would be a great addition to your retirement portfolio at {5}."
+template_up = "{0} (TSX:{1}) rose from a 52-week low of ${2} to a 52-week high of ${3} after the March 2020 market sell-off. At the time of writing, the stock is trading for ${4} per share. The dividend yield is {5} annually."
 
 
-symbol_list = ["T.TO", "RCI-B.TO", 'BCE.TO']
+symbol_list = ["PPL.TO", "TRP.TO", "IPL.TO"]
 template = template_down
 
 for each in symbol_list:
@@ -37,4 +37,4 @@ for each in symbol_list:
     description = info.description
     
     
-    print(template.format(symbol, symbol, low, high, current_price, dividend) + description)
+    print(template.format(symbol, symbol.strip(".TO"), low, high, current_price, dividend) + description)
