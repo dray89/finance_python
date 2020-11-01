197a4cf02315d3e7c2e11dbbe0eb2eebf0e2145c
Debra Ray <raydebra89@gmail.com>
2020-11-01 16:26:08 -0500

    added headers.json to gitignore (sensitive data)

--- tdameritrade_api/price_history.py
+++ tdameritrade_api/price_history.py
@@ -6,6 +6,7 @@ Created on Tue Nov 26 04:58:54 2019
 """
 from datetime import date, timedelta
 import requests
+import json 
 
 class get_price_history:
     '''
@@ -14,15 +15,18 @@ class get_price_history:
     PeriodType: Valid values are day, month, year, ytd
     Period: The number of periods to show. day: 1,2,3,4,5,10
     '''
-    def __init__(self, apikey, startDate,endDate=date.today() - timedelta(1), 
+    header_file = open("C:/Users/rayde/iCloudDrive/GitHub/finance_python/tdameritrade_api/headers.json", 'r')
+    hdrs = json.loads(header_file.read())
+    
+    def __init__(self, symbol, apikey=None, startDate=None,endDate=date.today() - timedelta(1), 
                  periodType='day', period=10, frequencyType='minute' , frequency=1,
                   needExtendedHoursData='true'):
-        self.periodtype  = periodType
-    
-        
-    def price_history(self, symbol):       
+        self.symbol = symbol
         self.url = 'https://api.tdameritrade.com/v1/marketdata/'+ symbol + '/pricehistory'
-        response = requests.get(self.url)
-        return response
+        self.periodtype  = periodType
+        
+    def price_history(self):
+        response = requests.get(self.url, headers=self.hdrs)
+        return response.content
         
         
